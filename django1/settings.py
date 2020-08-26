dde4ba89d7551d4cc7959c04a2e65fac61a36290
Daivith Oliveira <dwsantosgp@gmail.com>
2020-08-26 09:25:50 -0300

    att do allowed hosts e fix erro middleware

--- django1/settings.py
+++ django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0mlutf6^bv3h48^1r*5yvv^uejfls+i*6ektt6-q+i4miugrq!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django1-dw.herokuapp.com/']
 
 
 # Application definition
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
